b054ed85d763b2a677c747a2d8fd62560f66b1db
Hankscr459 <defcw58@gmail.com>
2021-04-12 15:10:10 +0800

    Creating dummy data with relationships - Users with Posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = User::Factory()->count(10)
+            ->has(Post::factory()->count(1))
+            ->create();
     }
 }
